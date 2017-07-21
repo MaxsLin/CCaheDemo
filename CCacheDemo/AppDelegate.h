bbcad0d92d6d4376ed1b9cfd12d2ee7d3fd6990e
MaxMak <maxmak@MaxMak.local>
2017-07-21 11:23:17 +0800

    create

---
+++ CCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCacheDemo
+//
+//  Created by MaxMak on 21/07/2017.
+//  Copyright © 2017 MaxMak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCacheDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCacheDemo
+//
+//  Created by MaxMak on 21/07/2017.
+//  Copyright © 2017 MaxMak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
